a1abe8881e9286812ae7d735a220108e41d77cc8
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-12-08 09:00:45 +0000

    Update year on copyright statement. [ci skip]

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pybtex-docutils'
-copyright = u'2013, Matthias C. M. Troffaes'
+copyright = u'2013-2014, Matthias C. M. Troffaes'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
