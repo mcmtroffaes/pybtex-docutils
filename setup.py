bb27d58b09d6d0e9512f9ef3e1cff3a00948c8ca
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-01-15 14:49:53 +0000

    Update python_requires to reflect that Python 3.4 is no longer supported.

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setup(
             'docutils = pybtex_docutils:Backend',
         ]
     },
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
 )
