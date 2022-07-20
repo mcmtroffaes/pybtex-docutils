8358c1b476d361a20bdb2e95ce304b90707551d7
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2022-07-20 12:42:27 +0100

    Support tests on docutils < 0.18 (fixes issue #22).

--- test/test_simplebibliography.py
+++ test/test_simplebibliography.py
@@ -1,4 +1,5 @@
 import re
+import docutils
 from docutils.core import publish_file
 import docutils.parsers.rst.directives as directives
 
@@ -6,24 +7,48 @@ from pybtex_docutils import SimpleBibliography
 
 
 def html_citation_ref(key):
-    return re.compile(
-        fr'<a class="citation-reference" '
-        fr'href="#{key.lower()}" id="[a-zA-Z0-9_-]+" '
-        fr'role="doc-biblioref">'
-        fr'\[{key}]'
-        fr'</a>')
+    if docutils.__version_info__ < (0, 18):
+        return re.compile(
+            fr'<a class="citation-reference" '
+            fr'href="#{key.lower()}" id="\w+">'
+            fr'\[{key}]'
+            fr'</a>')
+    else:
+        return re.compile(
+            fr'<a class="citation-reference" '
+            fr'href="#{key.lower()}" id="[a-zA-Z0-9_-]+" '
+            fr'role="doc-biblioref">'
+            fr'\[{key}]'
+            fr'</a>')
 
 
 def html_citation(key):
-    return re.compile(
-        fr'<div class="citation" id="{key.lower()}" role="doc-biblioentry">\s*'
-        fr'<span class="label">'
-        fr'<span class="fn-bracket">\[</span>'
-        fr'(?:<a role="doc-backlink" href="#[a-zA-Z0-9_-]+">)?'
-        fr'{key}'
-        fr'(?:</a>)?'
-        fr'<span class="fn-bracket">]</span>'
-        fr'</span>')
+    if docutils.__version_info__ < (0, 18):
+        return re.compile(
+            fr'<dt class="label" id="{key.lower()}">'
+            fr'<span class="brackets">'
+            fr'(?:<a class="fn-backref" href="#\w+">)?'
+            fr'{key}'
+            fr'(?:</a>)?'
+            fr'</span>'
+            fr'(?:<span class="fn-backref">\('
+            fr'<a href="#\w+">1</a>'
+            fr',<a href="#\w+">2</a>'
+            fr'(,<a href="#\w+">3</a>)?'
+            fr'(,<a href="#\w+">\d+</a>)*'
+            fr'\)</span>)?'
+            fr'</dt>\n')
+    else:
+        return re.compile(
+            fr'<div class="citation" id="{key.lower()}" '
+            fr'role="doc-biblioentry">\s*'
+            fr'<span class="label">'
+            fr'<span class="fn-bracket">\[</span>'
+            fr'(?:<a role="doc-backlink" href="#[a-zA-Z0-9_-]+">)?'
+            fr'{key}'
+            fr'(?:</a>)?'
+            fr'<span class="fn-bracket">]</span>'
+            fr'</span>')
 
 
 def test_simplebibliography(test_roots):
