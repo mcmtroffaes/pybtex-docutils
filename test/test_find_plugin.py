a0d812a84a4289ea29cfd79db1c90deb0aa58d1b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2023-08-22 21:10:22 +0200

    Update formatting.

--- test/test_find_plugin.py
+++ test/test_find_plugin.py
@@ -2,12 +2,14 @@
 # so its entry points can be found
 
 import pytest
+
 import pybtex_docutils
 
 
 @pytest.mark.plugin
 def test_pkg_resources_entry_point():
     from pkg_resources import iter_entry_points
+
     for ep in iter_entry_points("pybtex.backends", "docutils"):
         assert ep.load() is pybtex_docutils.Backend
 
@@ -15,5 +17,5 @@ def test_pkg_resources_entry_point():
 @pytest.mark.plugin
 def test_pybtex_find_plugin():
     from pybtex.plugin import find_plugin
-    assert (
-        find_plugin("pybtex.backends", "docutils") is pybtex_docutils.Backend)
+
+    assert find_plugin("pybtex.backends", "docutils") is pybtex_docutils.Backend
--- test/test_install_example.py
+++ test/test_install_example.py
@@ -1,7 +1,7 @@
 expected_result = (
-    '<paragraph>D.\xa0Lindley. <emphasis>Making Decisions</emphasis>. '
-    'Wiley, 2nd edition, 1985.</paragraph>'
-    )
+    "<paragraph>D.\xa0Lindley. <emphasis>Making Decisions</emphasis>. "
+    "Wiley, 2nd edition, 1985.</paragraph>"
+)
 
 
 def test_install_example():
@@ -9,13 +9,16 @@ def test_install_example():
 
     # example begin
     import io
+
     import pybtex.database.input.bibtex
     import pybtex.plugin
 
-    style = pybtex.plugin.find_plugin('pybtex.style.formatting', 'plain')()
-    backend = pybtex.plugin.find_plugin('pybtex.backends', 'docutils')()
+    style = pybtex.plugin.find_plugin("pybtex.style.formatting", "plain")()
+    backend = pybtex.plugin.find_plugin("pybtex.backends", "docutils")()
     parser = pybtex.database.input.bibtex.Parser()
-    data = parser.parse_stream(io.StringIO(u"""
+    data = parser.parse_stream(
+        io.StringIO(
+            """
     @Book{1985:lindley,
       author =    {D. Lindley},
       title =     {Making Decisions},
@@ -23,10 +26,12 @@ def test_install_example():
       year =      {1985},
       edition =   {2nd},
     }
-    """))
+    """
+        )
+    )
     for entry in style.format_entries(data.entries.values()):
         print(backend.paragraph(entry))
-    # example end
+        # example end
         result.append(backend.paragraph(entry))
 
     assert len(result) == 1
